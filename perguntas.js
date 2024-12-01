042b577a4817cdb77ce865172f05b4e295264bdd
arkizu9 <00001113865040sp@al.educacao.sp.gov.br>
2024-12-01 16:56:20 -0300

    adicionado os aquivos

---
+++ perguntas.js
@@ -0,0 +1,44 @@
+criaCartao (
+    'programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função?',
+    'Função é um bloco de código que executa alguma tarefa'
+)
+
+
+
+criaCartao (
+    'lingua inglesa',
+    'como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
+
+criaCartao (
+    'Historia',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci, entre 1503 e 1506, em Florença, na Itália'
+)
+
+criaCartao (
+    'Computação' ,
+    'O que é maior: gigabyte ou megabyte' ,
+    'O gigabyte é maior do que o megabyte. Um gigabyte equivale a 1024 megabytes.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'Ottawa'
+)
+
